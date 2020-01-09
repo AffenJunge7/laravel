afdbc7f7bc7f7e19d45fb8327183a4669577f02f
AffenJunge7 <32170053+AffenJunge7@users.noreply.github.com>
2020-01-09 20:46:46 +0100

    new

--- resources/views/pages/auftraege.blade.php
+++ resources/views/pages/auftraege.blade.php
@@ -2,7 +2,13 @@
 
 @section('title', 'Page Title')
 
+
+
 @section('content')
-    @include('components.sidebar')
-    <p>This is my body content.</p>
+    <div class="col-3">
+        @include('components.sidebar')
+    </div>
+    <div class="col-9">
+        <p>This is my body content.</p>
+    </div> 
 @endsection
